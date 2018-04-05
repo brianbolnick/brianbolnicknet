fb0d135d69be8daa6df1fea9d6e1c0dfdfb5bf5a
Brian Bolnick <brianbolnick@gmail.com>
2018-04-04 23:35:41 -0600

    rebuild project show

--- src/components/Icpm.js
+++ src/components/Icpm.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Home from '../img/icpm-dashboard.png'
 import Layout from './Layout'
-import { Image, Modal, Divider, Grid } from 'semantic-ui-react';
-import Home2 from '../img/showcase-icpm-dashboard.png'
 import HomeFull from '../img/icpm-dashboard.png'
-import Notes from '../img/showcase-icpm-project-notes.png'
 import NotesFull from '../img/icpm-project-notes.png'
 import Tasks from '../img/showcase-icpm-project-tasks.png'
-import TasksFull from '../img/icpm-project-tasks.png'
+import Signup from '../img/showcase-icpm-signup.png'
+import Login from '../img/showcase-icpm-login.png'
+import Contacts from '../img/icpm-contacts.png'
+
 import erd from '../img/icpm-erd.png'
 
 class Icpm extends Component {
@@ -22,7 +22,7 @@ class Icpm extends Component {
                 </div>
                 <div className="showcase-main-content" id="showcase-main-content" ref="first">
                     <div className='single-showcase-container'>
-                        <Divider hidden style={{ marginBottom: '30px' }} />
+                        <div className="divider" style={{ marginBottom: '30px' }}></div>
                         <div className="showcase-background">
                             While working as an Implementation Consultant at Instructure, project management played a
                             huge role in successfully implementing new instances of the Canvas LMS. The team was previously
@@ -66,34 +66,82 @@ class Icpm extends Component {
                         <div className="showcase-section-title">Database Architecture</div>
                         <img src={erd} alt="icpm-erd" className="erd" />
                     </div>
-                    <Grid columns={2} doubling>
-                        <Divider hidden />
-                        <Grid.Row>
-                            <Grid.Column computer={8}>
-                                <Modal closeIcon trigger={<Image src={Home2} fluid className="main-showcase-img" />}>
-                                    <Image src={HomeFull} fluid />
-                                </Modal>
-                            </Grid.Column>
-                            <Grid.Column computer={8}>
-                                <Modal closeIcon trigger={<Image src={Tasks} fluid className="main-showcase-img" />}>
-                                    <Image src={TasksFull} fluid />
-                                </Modal>
-                            </Grid.Column>
-                            <Grid.Column computer={8}>
-                                <Modal closeIcon trigger={<Image src={Notes} fluid className="main-showcase-img" />}>
-                                    <Image src={NotesFull} fluid />
-                                </Modal>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row style={{ marginBottom: '100px' }}>
-                            <Grid.Column textAlign='center' style={{ textAlign: 'center', width: '100%' }}>
-                                <a href="https://icpm.herokuapp.com/" className="showcase-link">VISIT THE SITE</a>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Divider hidden />
-                    </Grid>
                 </div>
-
+                <div className="showcase-third">
+                    <div className="authentication">
+                        <div className="showcase-split-title">Authentication</div>
+                        <div className="showcase-split-text">
+                            ICPM implements local authentication with the Passport NPM. The app is held within a single
+                            file structure, but the server and client are two separate applications. For this purpose, I also
+                            implemented JWTs to validate and authenticate HTTP requests from the client to the server.
+                            <br /><br />
+                            The User's registration email is saved into the database with a salted and hashed password. Each login
+                            attempt is compared to the hashed value, which then uses a Redux <code>push</code> to forward the user to
+                            the main dashboard page.
+                        </div>
+                    </div>
+                    <div className="showcase-double-img">
+                        <img src={Signup} alt="" className="double-img"/>
+                        <img src={Login} alt=""  className="double-img"/>
+                    </div>
+                </div>
+                <div className="showcase-fourth">
+                    <div className="feature-left">
+                        <div className="showcase-split-text">
+                            The dashboard (or home page) lists each project belonging to the Implementation Consultant. As this information
+                           is stored in the database, a Redux <code>fetch</code> action is dispatched when the page loads to retrieve all
+                              projects belonging to the authenticated user.
+                           <br /><br />
+                            Each card displays general information about the project, as well
+                           as an outline of the progress for each implementation milestone. Clicking on a card brings you to the main project
+                           view page.
+                        </div>
+                        <div className="showcase-split-title">Dashboard</div>
+                    </div>
+                    <div className="showcase-single-img">
+                        <img src={HomeFull} alt="" width="100%" />
+                    </div>
+                </div>
+                <div className="showcase-fifth">
+                    <div className="feature-right">
+                        <div className="showcase-split-title">Project View</div>
+                        <div className="showcase-split-text">
+                            The project view shows specific information for a single project. The main view consists of
+                            an overview of implementation milestones (SIS, Branding, Authentication, Migration, and Other).
+                            Each sub-task of the milestones are able to be completed from this view, updating the overall milestone
+                           progress. The client sends an <code>HTTP PUT</code> request to the server to update the task status.
+                             More information about the project as a whole is also displayed.
+                        </div>
+                    </div>
+                    <div className="showcase-single-img">
+                        <img src={Tasks} alt="" className="single-img"/>
+                    </div>
+                </div>
+                <div className="showcase-sixth">
+                    <div className="feature-left">
+                        <div className="showcase-split-text">
+                            The other sections of a project view are the project contacts and note tracker. Contacts allows the
+                            user to add points of contact for the project with designated role and contact information. All of this
+                            data is easily visible, and allows the user to send an email directly to a contact if desired.
+                            <br /><br />
+                            The notes view is a collection of rich content editors that help to generate a
+                            <span style={{ fontWeight: "600" }}> Weekly Status Call</span> email template. I created this feature to help
+                            save time by granting the IC the ability to take all notes for the status call in the application itself and
+                            then easily convert it into an email that is sendable to the client. The email template also pulls in additional
+                            project information (dates, project folder links, etc.). My roadmap planned to include a feature that would allow
+                            you to select stored contacts before creating the template so it would pre-populate everything you need in order to
+                            send it off.
+                        </div>
+                        <div className="showcase-split-title">Contacts and Notes</div>
+                    </div>
+                    <div className="showcase-double-img">
+                        <img src={Contacts} alt=""  style={{ margin: '10px' }} className="double-img"/>
+                        <img src={NotesFull} alt="" style={{ margin: '10px' }} className="double-img"/>
+                    </div>
+                    <div className="visit-link">
+                        <a href="https://icpm.herokuapp.com" className="showcase-link-dark">VISIT THE SITE</a>
+                    </div>
+                </div>
             </Layout>
         )
     }
