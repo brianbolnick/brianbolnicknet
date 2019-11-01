d693ea585c0ae3f23c343912e1443d21fc0b65ff
Brian Bolnick <brianbolnick@gmail.com>
2019-11-01 09:15:21 -0600

    update humanize url

--- src/components/Home/HumanizeTime.js
+++ src/components/Home/HumanizeTime.js
@@ -5,7 +5,7 @@ import ProjectDetails from './Project/ProjectDetails';
 
 export default () => (
     <ProjectContainer>
-        <a href='//https://hex.pm/packages/humanize_time' target="_blank" rel="noopener noreferrer">
+	<a href='//hex.pm/packages/humanize_time' target="_blank" rel="noopener noreferrer">
             <div className="project-show">
                 <div className="parent-container" id='humanize'>
                     <div className="child-container" style={{ width: '450px', marginLeft: '0px' }}>
